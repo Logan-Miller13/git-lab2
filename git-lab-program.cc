f32285881e5241b3fbe4d0e5b9fc2b06f03293c7
Logan Miller <lm905123@ohio.edu>
2025-01-22 17:39:32 -0500

    committed

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Logan Miller
+ *        Date: 1/22/2025
+ * Description: Adding code for lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}
